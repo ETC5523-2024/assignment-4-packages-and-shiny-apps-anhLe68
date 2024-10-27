4cd80da1c08c977df4d1f14e1bec92089b0d4af4
Phuong Anh Le <plee0068@student.monash.edu>
2024-10-27 17:29:07 +1100

    Data help document

---
+++ R/data_1.R
@@ -0,0 +1,17 @@
+#' Number of coffee shops in Melbourne from 2002 and 2022
+#'
+#' @format ## 'vic_coffee_map'
+#' A data frame with 279 rows and 5 columns.
+#' \describe{
+#'   \item{postcode}{Suburb postcode}
+#'   \item{Location}{Name of the suburb}
+#'   \item{census year}{Census year (2002 to 2022)}
+#'   \item{cafe count}{Records of number of coffee business establishment in Melbourne}
+#'   \item{geometry}{Geomtric dimension of the suburb}
+#' }
+#' @source <https://data.melbourne.vic.gov.au/explore/dataset/cafes-and-restaurants-with-seating-capacity/export/?location=13,-37.8044,144.9387&basemap=mbs-7a7333>
+#' @source <https://www.arcgis.com/home/item.html?id=153afcf67da145fea4d2f21a69a7ef1d>
+#' @name vic_coffee_map
+#' @docType data
+
+"vic_coffee_map"
---
+++ R/data_2.R
@@ -0,0 +1,17 @@
+#' Median age and median income of Melbourne suburbs for the census year 2016 and 2021
+#'
+#' @format ## 'census_suburb'
+#' A data frame with 14 rows and 5 columns.
+#' \describe{
+#'   \item{Location}{Name of the suburb}
+#'   \item{Median_age_21}{Average age of the suburb in 2021}
+#'   \item{Median_income_21}{Average personal income of the suburb in 2021}
+#'   \item{Median_age_16}{Average age of the suburb in 2016}
+#'   \item{Median_income_16}{Average personal income of the suburb in 2016}
+#' }
+#' @source <https://www.abs.gov.au/census/find-census-data/datapacks?release=2021&product=GCP&geography=SAL&header=S>
+#' @source <https://www.abs.gov.au/census/find-census-data/datapacks?release=2016&product=GCP&geography=SSC&header=S>
+#' @name census_suburb
+#' @docType data
+
+"census_suburb"
